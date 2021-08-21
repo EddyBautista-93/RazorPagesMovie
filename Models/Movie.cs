67b5863a12b70cf0e8335d8b5f6f97345a2371fa
EddyBautista <bautista.j.eddy@gmail.com>
2021-08-21 10:51:04 -0500

    add movie.cs

---
+++ Models/Movie.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+namespace RazerPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id {get; set;} // Primary Key 
+        public string Title {get; set;}
+
+        [DataType(DataType.Date)] // specifies the type of data, with this a user
+                                  // is not required to enter time information in the data field
+        public DateTime ReleaseDate { get; set;}
+        public string Genre { get; set;}
+        public decimal Price { get; set;}
+
+    }
+}
